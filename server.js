0a989d0e68017051c65a962a4b228f2a12213bde
elitan <johan@eliasson.me>
2016-10-30 19:22:12 +0100

    allow CORS and send back http code 200 on options-request

--- server.js
+++ server.js
@@ -7,9 +7,19 @@ import { graphqlExpress, graphiqlExpress } from 'graphql-server-express';
 import { makeExecutableSchema, addMockFunctionsToSchema } from 'graphql-tools';
 import bodyParser from 'body-parser';
 
-const GRAPHQL_PORT = 8080;
+const GRAPHQL_PORT = 8081;
 
 const graphQLServer = express();
+graphQLServer.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    // intercept OPTIONS method
+    if ('OPTIONS' === req.method) {
+      res.send(200);
+    } else {
+      next();
+    }
+});
 
 const executableSchema = makeExecutableSchema({
   typeDefs: Schema,
